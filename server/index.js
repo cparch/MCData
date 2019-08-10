e2549667233dbe996fa0cdac9da3c2de139717c4
Chad Archila <chad@Chads-MacBook-Pro.local>
2019-08-10 14:51:31 -0700

    Add getData func to get request

--- server/index.js
+++ server/index.js
@@ -1,7 +1,8 @@
 const express = require('express')
 const app = express()
 const port = 3444
+const controller = require('./controller/controller')
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', controller.getData )
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
